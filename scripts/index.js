563c5fdc5d72cdc17b8bd46b9d57e0560daea4e0
Alex <lathamwyse123@gmail.com>
2022-11-09 14:56:00 -0800

    made a if statement stating if the courseList is true

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,22 @@
+let ACIT1620 ={
+    code:"ACIT 1620",
+    name:"Web Fundamental Technologies",
+};
+let ACIT1420 = {
+    code:"ACIT 1420", 
+    name:"System Administration",
+};
+let ACIT1515 = {
+    code:"ACIT 1515", 
+    name:"Scripting for IT",
+};
+
 const courseList = [
-{code:"ACIT 1620", name:"Web Fundamental Technologies"},
-{code:"ACIT 1420", name:"System Administration"},
-{code:"ACIT 1515", name:"Scripting for IT"}];
+ACIT1620,
+ACIT1420,
+ACIT1515];
+
+
+let userinput = prompt("Enter a 4 digit code: ")
+
+while (userinput != courseList.code.slice(4:))
